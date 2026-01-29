560a771271ef36895ba5a8370c37df8caed7eb6d
Hiiragi <hiiragi6991@gmail.com>
2026-01-29 20:40:20 +0900

    Use environment variable for YouTube API key

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, abort
 from googleapiclient.discovery import build
 import random
 
